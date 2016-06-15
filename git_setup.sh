c8dce2adbc0e9f4e5fe2e295face566c7c18792f
Michael Galloy <mgalloy@gmail.com>
2016-06-15 10:44:37 -0600

    Setup git for signing commits

--- git_setup.sh
+++ git_setup.sh
@@ -2,6 +2,7 @@
 
 git config --global user.name "Michael Galloy"
 git config --global user.email mgalloy@gmail.com
+git config --global user.signingkey A60F2AA0
 
 git config --global core.excludesfile ~/.gitignore_global
 git config --global core.editor "emacs -nw"
@@ -19,3 +20,5 @@ git config --global push.default matching
 
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.stat status
+
+git config --global commit.gpgsign true
